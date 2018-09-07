e9c8a15433b89a56dcd66446c7781e5901bcea9b
dheitinga <kiggdesign@gmail.com>
2018-09-07 20:19:30 +0200

    re-arranging of files bootstrap implementation

--- wp-content/themes/foodbyvalenc/index.php
+++ wp-content/themes/foodbyvalenc/index.php
@@ -1,19 +1,51 @@
-<?php get_header(); ?>
+<!doctype html>
+<html <?php language_attributes(); ?> class="no-js">
+	<head>
+		<meta charset="<?php bloginfo('charset'); ?>">
+		<title><?php wp_title(''); ?><?php if(wp_title('', false)) { echo ' :'; } ?> <?php bloginfo('name'); ?></title>
 
-	<main role="main">
-		<!-- section -->
-		<section>
+    <link href="<?php echo get_template_directory_uri(); ?>/img/icons/favicon.ico" rel="shortcut icon">
+    <link href="<?php echo get_template_directory_uri(); ?>/img/icons/touch.png" rel="apple-touch-icon-precomposed">
 
-			<h1><?php _e( 'Latest Posts', 'html5blank' ); ?></h1>
+		<meta http-equiv="X-UA-Compatible" content="IE=edge,chrome=1">
+		<meta name="viewport" content="width=device-width, initial-scale=1.0">
+		<meta name="description" content="<?php bloginfo('description'); ?>">
 
-			<?php get_template_part('loop'); ?>
+		<?php wp_head(); ?>
+		<script>
+	    // conditionizr.com
+	    // configure environment tests
+	    conditionizr.config({
+	        assets: '<?php echo get_template_directory_uri(); ?>',
+	        tests: {}
+	    });
+    </script>
 
-			<?php get_template_part('pagination'); ?>
+	</head>
+	<body <?php body_class(); ?>
 
-		</section>
-		<!-- /section -->
-	</main>
+		<!-- wrapper -->
+		<div class="wrapper">
+			<?php get_header(); ?>
 
-<?php get_sidebar(); ?>
+				<main role="main">
+					<!-- section -->
+					<section>
 
-<?php get_footer(); ?>
+						<h1><?php _e( 'Latest Posts', 'html5blank' ); ?></h1>
+
+						<?php get_template_part( 'partials/loop' ); ?>
+
+						<?php get_template_part( 'partials/pagination' ); ?>
+
+					</section>
+					<!-- /section -->
+				</main>
+
+			<?php get_footer(); ?>
+		</div>
+		<!-- /wrapper -->
+
+		<?php wp_footer(); ?>
+	</body>
+</html>
