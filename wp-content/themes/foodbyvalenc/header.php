05b3ccc42c1983c57ef4fd693a75225e542d30d8
dheitinga <kiggdesign@gmail.com>
2019-03-10 20:00:44 +0100

    header hotfixes

--- wp-content/themes/foodbyvalenc/header.php
+++ wp-content/themes/foodbyvalenc/header.php
@@ -1,15 +1,15 @@
 <!-- header -->
 <header class="header clear" role="banner">
-	<div class="container-fluid d-flex">
-		<div class="intro">
+	<div class="container-fluid">
+		<div class="row justify-content-center intro">
 			<?php if (is_home()): ?>
-				<h1>
+				<h1 class="text-center">
 					Hoi! Ik ben <span>Valencia</span>, <br />
 					mijn passies zijn eten <br />
 					en koken <br />
 				</h1>
 			<?php else: ?>
-				<h1>
+				<h1 class="text-center">
 					<?php echo the_title(); ?>
 				</h1>
 			<?php endif; ?>
